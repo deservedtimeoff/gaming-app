d7483a55cc11363d257c30dd8cca0859f2cf5d29
shwarnock <shwarnock89@gmail.com>
2024-12-17 14:48:17 -0600

    test

--- login-server/server.js
+++ login-server/server.js
@@ -4,9 +4,21 @@ const app = require('express')();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
-app.use(cors({
-    origin: '*'
-}));
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 
 const UserRouter = require('./api/User');
 
