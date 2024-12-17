225e2af3f6248f0828545d6cfc529c320e7d3e29
shwarnock <shwarnock89@gmail.com>
2024-12-17 09:06:21 -0600

    Fixing signup issues

--- login-server/config/db.js
+++ login-server/config/db.js
@@ -1,10 +1,7 @@
 ﻿require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI, {})
     .then(() => {
         console.log('DB Connected');
     })
