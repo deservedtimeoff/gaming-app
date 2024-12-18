95de6061c2ab5321dcd15dfc2e8090e8e10eaf24
shwarnock <shwarnock89@gmail.com>
2024-12-18 17:53:44 -0600

    Updating temp logging

--- login-server/api/utils/checkJWT.js
+++ login-server/api/utils/checkJWT.js
@@ -2,10 +2,13 @@
 
 exports.cookieJwtAuth = (req, res, next) => {
     const token = req.cookies.token;
+    console.log('I started');
     jwt.verify(token, process.env.MY_SECRET).then((data) => {
         req.user = data;
         next();
+        console.log('I succeeded');
     }).catch((err) => {
+        console.log('I Failed');
         res.clearCookie("token");
         res.json({
             status: "FAILED",
