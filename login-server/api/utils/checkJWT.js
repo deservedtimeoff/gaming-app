8dc87fae7a5c74500e19cc1f7c5da90770f4b357
shwarnock <shwarnock89@gmail.com>
2024-12-18 17:33:10 -0600

    Update cookie authentication

--- login-server/api/utils/checkJWT.js
+++ login-server/api/utils/checkJWT.js
@@ -2,6 +2,7 @@
 
 exports.cookieJwtAuth = (req, res, next) => {
     const token = req.cookies.token;
+    console.log(token);
     try {
         console.log('Here');
         req.user = jwt.verify(token, process.env.MY_SECRET);
