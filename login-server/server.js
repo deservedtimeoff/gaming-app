e6c705ec135960bc6fa237a949746167f2b38780
shwarnock <shwarnock89@gmail.com>
2024-12-19 11:23:15 -0600

    Test

--- login-server/server.js
+++ login-server/server.js
@@ -3,13 +3,11 @@
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
-    res.setHeader("Access-Control-Allow-Headers", "Content-type");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-})
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost:8081',
+    credentials: true
+}))
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
