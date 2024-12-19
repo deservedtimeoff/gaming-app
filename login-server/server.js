8ff9594d8b434d53917f7fa6bb6285aa52d4cbfb
shwarnock <shwarnock89@gmail.com>
2024-12-19 13:35:18 -0600

    Test

--- login-server/server.js
+++ login-server/server.js
@@ -10,6 +10,11 @@ app.use(cors({
 
 }))
 
+app.use((req, res, next) => {
+    app.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081');
+    next();
+});
+
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
