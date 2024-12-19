5d576030746b411db01a6d63fe0521d9f223e390
shwarnock <shwarnock89@gmail.com>
2024-12-18 19:56:47 -0600

    Remove temp logging

--- login-server/api/utils/checkJWT.js
+++ login-server/api/utils/checkJWT.js
@@ -3,23 +3,17 @@
 exports.cookieJwtAuth = (req, res, next) => {
     const token = req.cookies.token;
     try {
-        console.log('I am here');
         if (token) {
-            console.log('I am here2');
             req.user = jwt.verify(token, process.env.MY_SECRET);
             next();
-            console.log('I am here3');
         }
         else {
-            console.log('I am here4');
             res.json({
                 status: "FAILED",
                 message: "No token provided!"
             })
         }
     } catch (err) {
-        console.log('I am here5');
-        console.log('Or here');
         res.clearCookie("token");
         res.json({
             status: "Failed",
